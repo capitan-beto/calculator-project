b0715c8b684ab97408299cc771df35ff1a856317
Otroletravaladna <crnana98@gmail.com>
2022-10-06 01:50:24 -0300

    Create basic opeations functions

---
+++ script.js
@@ -0,0 +1,23 @@
+// creating operators functions
+
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract (a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+console.log(add(3, 5));
+console.log(subtract(9, 1));
+console.log(multiply(4, 2));
+console.log(divide(16, 2));
