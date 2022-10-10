30f02c2ab5abf66b49ee464137272c258824d61d
Otroletravaladna <crnana98@gmail.com>
2022-10-10 01:02:26 -0300

    Add operation draft

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const a = [];
-const b = [];
+let input = "";
+let result = ""
 
 
 
@@ -10,16 +10,30 @@ function add(a, b){
 }
 
 function subtract (a, b){
-    return a - b;
+    return splitStr[0] - splitStr[1];
 }
 
 function multiply(a, b){
-    return a * b;
+    return splitStr[0] * splitStr[1];
 }
 
-function divide(a, b){
-    return a / b;
+/*function divide(){
+    
+    let divOne = splitStr[divIndex - 1];
+    let divTwo = splitStr[divIndex + 1];
+    return divOne / divTwo;
+}*/
+
+function operation(input) {
+    let splitStr = input.split(",");
+    const divIndex = splitStr.indexOf("/");
+    let divOne = divIndex - 1;
+    let divTwo = divIndex + 1;
+    let result = splitStr[divOne] / splitStr[divTwo];
+    console.log(splitStr);
+    console.log(result);
 }
+    
 
 
 // Display:
@@ -34,92 +48,109 @@ prevOperation.textContent = "I'M ALIVE TOO";
 
 const zero = document.querySelector(".zero");
 zero.addEventListener("click", () => {
+    input += 0;
     currOperation.textContent += 0;
 })
 
 const one = document.querySelector(".one");
 one.addEventListener("click", () => {
+    input += 1;
     currOperation.textContent += 1;
 })
 
 const two = document.querySelector(".two");
 two.addEventListener("click", () => {
+    input += 2;
     currOperation.textContent += 2;
 })
 
 const three = document.querySelector(".three");
 three.addEventListener("click", () => {
+    input += 3;
     currOperation.textContent += 3;
 })
 
 const four = document.querySelector(".four");
 four.addEventListener("click", () => {
+    input += 4; 
    currOperation.textContent += 4;
 }) 
 
 const five = document.querySelector(".five");
 five.addEventListener("click", () => {
-currOperation.textContent += 5;
+    input += 5;
+    currOperation.textContent += 5;
 })
 
 const six = document.querySelector(".six");
 six.addEventListener("click", () => {
+    input += 6;
    currOperation.textContent += 6;
 })
 
 const seven = document.querySelector(".seven");
 seven.addEventListener("click", () =>{
+    input += 7;
     currOperation.textContent += 7;
 })
 
 const eight = document.querySelector(".eight");
 eight.addEventListener("click", () =>{
+    input += 8;
     currOperation.textContent += 8;
 })
 
 const nine = document.querySelector(".nine");
 nine.addEventListener("click", () =>{
+    input += 9;
     currOperation.textContent += 9;
 })
 
 const equal = document.querySelector(".equal");
 equal.addEventListener("click", () =>{
     currOperation.textContent += "=";
+    operation(input);
 })
 
 const sum = document.querySelector(".sum");
 sum.addEventListener("click", () =>{
+    input += ",+,";
     currOperation.textContent += "+";
 })
 
 const minus = document.querySelector(".minus");
 minus.addEventListener("click", () =>{
+    input += ",-,";
     currOperation.textContent += "-";
 })
 
 const dot = document.querySelector(".dot");
 dot.addEventListener("click", () => {
+    input += ".";
     currOperation.textContent += ".";
 })
 
 const multi = document.querySelector(".multiply");
 multi.addEventListener("click", () => {
+    input += ",x,";
     currOperation.textContent += "x";
 })
 
 const division = document.querySelector(".divide");
 division.addEventListener("click", () => {
+    input += ",/,";
     currOperation.textContent += "÷";
 })
 
 const clean = document.querySelector(".clean");
 clean.addEventListener("click", () => {
+    input = "";
     currOperation.textContent = "";
 })
 
 const puto = document.querySelector(".puto");
 puto.addEventListener("click", () => {
-   return "puto el que lee";
+   currOperation.textContent = "puto el que lee";
 })
 
 const undef = document.querySelector(".undef");
