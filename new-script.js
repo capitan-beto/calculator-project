0e083954f8552326e9ef7010b4a2e290744c16d0
Otroletravaladna <crnana98@gmail.com>
2022-10-20 01:12:42 -0300

    Set spiderman background when '?' button is pressed

--- new-script.js
+++ new-script.js
@@ -88,12 +88,11 @@ function sum(){
 }
 
 
-/*const themeToggleBtn = document.querySelector(".theme-toggler");
-const calculator = document.querySelector(".calculator");
-const toggleIcon = document.querySelector(".toggler-icon");
-let isDark = true;
-themeToggleBtn.onclick = () => {
-    calculator.classList.toggle("dark");
-    themeToggleBtn.classList.toggle("active");
-    isDark = !isDark; 
-}*/
+const toggleSpidey = document.querySelector("#puto");
+toggleSpidey.onclick = () => {
+    document.body.style.background = "url('../calculator-project/images/ok.jpg')";
+    upperDisplay.innerText = "puto"
+    display.innerText = "el que lee";
+    setTimeout(() => (document.body.style.background = "",
+    upperDisplay.innerText = "", display.innerText = ""), 1000);
+}
